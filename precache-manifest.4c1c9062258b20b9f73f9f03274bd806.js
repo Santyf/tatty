2cc0e917e880e623c393872097999b7602f5d63b
Santiago <santyf@live.com>
2020-02-11 20:13:41 -0500

    Updates

---
+++ precache-manifest.4c1c9062258b20b9f73f9f03274bd806.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f084493b935639f518b876bec6aa58ec",
+    "url": "/tatty/index.html"
+  },
+  {
+    "revision": "ba3a7aaa939c218ba5f9",
+    "url": "/tatty/static/css/main.2d53ecb5.chunk.css"
+  },
+  {
+    "revision": "761e3547bbf9b39435a5",
+    "url": "/tatty/static/js/2.fa3e7651.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/tatty/static/js/2.fa3e7651.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba3a7aaa939c218ba5f9",
+    "url": "/tatty/static/js/main.934d6fbc.chunk.js"
+  },
+  {
+    "revision": "487988a0bff937bdc426",
+    "url": "/tatty/static/js/runtime-main.f4368d07.js"
+  },
+  {
+    "revision": "df737b8598dbbfb2d4cd925979689292",
+    "url": "/tatty/static/media/Overwatch.df737b85.jpg"
+  }
+]);
